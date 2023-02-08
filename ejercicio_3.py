a288c3ef264d46218830e748e83818014356d30b
DDGUZMANO <douglasguzmanorence29@gmail.com>
2023-02-08 16:27:57 +0100

    agregando un condicional if

--- ejercicio_3.py
+++ ejercicio_3.py
@@ -1,9 +1,13 @@
 nombre = input("Hola, ingrese su nombre por favor: ")
-peso = int(input(f'hola {nombre} ingresa tu peso en Kg por favor: '))
-estatura = float(input (f'ahora ingressa tu estatura en metros por favor: '))
-print('tu indice de masa corporal es: ')
-imc = peso / (estatura**2)
-print(round(imc,2))
+respuesta = input(f"Que bueno que estas acá. {nombre}, Deseas conocer su indice de masa corporal??: ")
+if respuesta == "si" or respuesta =="Si" or respuesta == "SI" or respuesta == "sI":
+    peso = int(input(f'{nombre} ingresa tu peso en Kg por favor: '))
+    estatura = float(input (f'ahora ingressa tu estatura en metros por favor: '))
+    print('tu indice de masa corporal es: ')
+    imc = peso / (estatura**2)
+    print(round(imc,2))
+    
+else: print("vuelva cuando tenga ganas de conocer su IMC")
 
 
 
